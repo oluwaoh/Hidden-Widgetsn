72d4f8263e4c5f4c6bd6030f3b08bb618770b759
Aryan Chaubal <aryanchaubal@icloud.com>
2020-08-24 16:45:38 +0530

    Fixed a bug where the image would not load into the widget

--- EmptySpaceWidget/ImageLoader.swift
+++ EmptySpaceWidget/ImageLoader.swift
@@ -12,7 +12,10 @@ class ImageLoader {
     
     func getImage(with name: String) -> UIImage? {
         let fileManager = FileManager.default
-        guard let bundleId = Bundle.main.bundleIdentifier else { return nil }
+        guard var bundleId = Bundle.main.bundleIdentifier else { return nil }
+        print("loading", bundleId)
+        let bundleIdComponents = bundleId.split(separator: ".")
+        bundleId = bundleIdComponents[0] + "." + bundleIdComponents[1] + "." + bundleIdComponents[2]
         guard let url = fileManager.containerURL(forSecurityApplicationGroupIdentifier: "group.\(bundleId)")?.appendingPathComponent("\(name).png") else { return nil }
         if fileManager.fileExists(atPath: url.path) {
             let image = UIImage(contentsOfFile: url.path)
